13aa745e7650dad1e83c1967b4f3208f25a31552
Rav1xRav1 <112080728+Rav1xRav1@users.noreply.github.com>
2023-02-06 19:33:39 +0900

    層の構成を変更した

--- src/model.py
+++ src/model.py
@@ -2,15 +2,18 @@ from torch import nn
 from torch import optim
 
 
-class model(nn.Module):
+class Model(nn.Module):
     def __init__(self):
         super().__init__()
 
         self.relu = nn.ReLU()  # 活性化関数
 
-        self.conv1 = nn.Conv2d(3, 6, 2)  # 畳み込み層
+        self.conv1 = nn.Conv2d(3, 9, 4)  # 畳み込み層
+        self.conv2 = nn.Conv2d(9, 15, 4)
 
-        self.fc1 = nn.Linear(1512, 1024)  # 全結合層
+        self.pool = nn.MaxPool2d(3, 1)  # プーリング層
+
+        self.fc1 = nn.Linear(900, 1024)  # 全結合層
         self.fc2 = nn.Linear(1024, 512)
         self.fc3 = nn.Linear(512, 512)
         self.fc4 = nn.Linear(512, 20*16)
@@ -19,15 +22,16 @@ class model(nn.Module):
 
         # 損失関数と最適化関数
         self.criterion = nn.MSELoss()
-        # self.optimizer = optim.Adam(self.parameters())
-        self.optimizer = optim.RMSprop(self.parameters())
+        self.optimizer = optim.Adam(self.parameters())
+        # self.optimizer = optim.RMSprop(self.parameters())
 
     def forward(self, x):
-        print(x)
-        x = self.conv1(x)
-        x = self.relu(x)
+        x = self.relu(self.conv1(x))
+        x = self.pool(x)
+        x = self.pool(self.relu(self.conv2(x)))
         x = x.view(x.size()[0], -1)
         x = x.view(-1)
+        print(x.size())
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
