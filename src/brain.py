eddb8a6ea8cf965f34a8683c728d6783374fa04a
Rav1xRav1 <112080728+Rav1xRav1@users.noreply.github.com>
2023-02-06 19:32:51 +0900

    GPUデバイスをCUDAからdirectMLに変更

--- src/brain.py
+++ src/brain.py
@@ -2,16 +2,21 @@ import numpy as np
 
 import torch
 
+from model import Model
+
 
 class Brain:
-    def __init__(self, net, is_gpu, gamma=0.9, r=0.9999999, lr=0.01, debug=False):
+    def __init__(self, net, is_gpu, gamma=0.9, r=0.99, lr=0.01, debug=False, device="cpu"):
         self.n_state = 5 * 5  # 状態の数
         self.n_action = 20*16  # 行動の数
 
-        self.net = net  # ニューラルネットワークのモデル
+        self.net: Model = net  # ニューラルネットワークのモデル
         self.is_gpu = is_gpu  # GPUを使うかどうか
+        self.device = device
+
         if self.is_gpu:
-            self.net.cuda()  # GPU対応
+            self.net.to(self.device)  # GPU対応
+            # print(self.net)
 
         self.eps = 1.0  # ε
         self.gamma = gamma  # 割引率
@@ -24,8 +29,12 @@ class Brain:
 
         states = torch.from_numpy(states).float()  # テンソルに変換
         if next_states is not None: next_states = torch.from_numpy(next_states).float()  # テンソルに変換
+
         if self.is_gpu:
-            states, next_states = states.cuda(), next_states.cuda()  # GPU対応
+            states = states.to(self.device)
+
+            if next_states is not None:
+                next_states = next_states.to(self.device)
 
         self.net.eval()  # 評価モード
         if next_states is not None: next_q = self.net(next_states)  # 次の場面をnnに通して値をとる
@@ -36,10 +45,6 @@ class Brain:
         if terminal:
             t[action] = reward  # エピソード終了時の正解は、報酬のみ
         else:
-            # if self.debug: print(next_q.detach().cpu().numpy())
-            # if self.debug: print(np.max(next_q.detach().cpu().numpy(), axis=0))
-            # print("t[action]=", t[action])
-            # print("追加する値:", reward + self.gamma * np.max(next_q.detach().cpu().numpy(), axis=0))
             t[action] = reward + self.gamma * np.max(next_q.detach().cpu().numpy(), axis=0)
 
         self.net.optimizer.zero_grad()  # 勾配を初期化
@@ -50,7 +55,7 @@ class Brain:
     def get_action(self, states):  # 行動を取得
         states = torch.from_numpy(states).float()
         if self.is_gpu:
-            states = states.cuda()  # GPU対応
+            states = states.to(self.device)  # GPU対応
 
         if np.random.rand() < self.eps:  # ランダムな行動
             action = np.random.randint(self.n_action)
@@ -60,8 +65,7 @@ class Brain:
             q = self.net(states)
             # softmaxを嚙ませたうえで行列の一番値が大きい要素のインデックスを返す
             action = np.argmax(q.detach().cpu().numpy(), axis=0)
-            # print(action)
+
         if self.eps > 0.1:  # εの下限
             self.eps *= self.r
-        # print(action)
         return action
