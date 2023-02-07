e0c0df0645353812545d355b0e073acd081e21a3
Rav1xRav1 <112080728+Rav1xRav1@users.noreply.github.com>
2023-02-07 22:56:35 +0900

    駒の頭の数値だけ規定より大きくした

--- src/agent.py
+++ src/agent.py
@@ -52,16 +52,19 @@ class Agent:
                             past_states = np.array(self.tron.get_memorize_board_info())
                             if debug: self.tron.print_board()  # ボード表示
                             if debug: print("正しい場所に駒を置けました。")
-                            # if debug: print("この状態から", past_states)
-                            # if debug: print("この状態に変化", now_states)
-                            # if debug: print("価値は0")
+                            if debug: print("この状態から")
+                            self.tron.print_board(board=past_states, is_debug=True)
+                            if debug: print("この状態に変化")
+                            self.tron.print_board(board=now_states, is_debug=True)
+                            if debug: print("価値は0")
                             self.brain.train(past_states, now_states, action, reward=0, is_finished=False)
                             self.tron.change_turn()  # 順番を交代
                             can_put = True  # 駒が置けました
                         else:  # 駒が置けないなら
                             if debug: print("正しい場所に駒を置けませんでした。")
-                            # if debug: print("この状態での", now_states)
-                            # if debug: print("このアクションが原因です", action)
+                            if debug: print("この状態での")
+                            self.tron.print_board(board=now_states, is_debug=True)
+                            if debug: print("このアクションが原因です:", action)
                             self.brain.train(now_states, past_states, action, reward=-1, is_finished=True)  # 第二引数意味なし
                             self.reset()  # 試合終了ボード初期化
                             done = True  # 処理終了
@@ -72,17 +75,16 @@ class Agent:
 
                 else:  # False: 動けない
                     if debug: print("駒が動けませんでした。")
-                    # print("この状態での", past_states)
-                    # print("この行動が原因でした", self.env.get_before_action(1))
+                    print("この状態での")
+                    self.tron.print_board(board=past_states, is_debug=True)
+                    print("この行動が原因でした", self.env.get_before_action(1))
                     self.brain.train(past_states, past_states, self.tron.memory.get_memory_one_action(), reward=-1, is_finished=True)  # 第二引数意味なし
-                    # print(self.env.get_before_action(1))
                     self.reset()  # 試合終了ボード初期化
                     done = True  # 処理終了
                     self.lose += 1
                     continue
             else:
                 if self.tron.can_move():
-                    # action = randint(0, 16 * 20 - 1)
                     action = -1
 
                     while not self.tron.put_two_koma(action):
